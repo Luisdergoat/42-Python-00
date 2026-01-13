953db35d1c4119ee0acb780c2ed05878319c43b2
Luis Unsold <lunsold@student.42heilbronn.de>
2026-01-13 18:59:44 +0100

    Norm

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,9 +1,10 @@
-def ft_count_harvest_recursive(current, days):
+def ft_count_harvest_recursive(current=1, days=None):
     """Count days until harvest recursively."""
-    if (current > days):
+    if (days is None):
+        days = int(input("Enter number of days until harvest: "))
+    print(f"Day {current}")
+    if (current >= days):
         print("Harvest time!")
         return
-    else:
-        print(f"Day {current}")
     ft_count_harvest_recursive(current + 1, days)
     pass
